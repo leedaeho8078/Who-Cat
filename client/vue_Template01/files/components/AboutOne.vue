f3b54c01d47368eee7832153a8d4a81055eb355d
JaeseungJung <86044304+JaeseungJung@users.noreply.github.com>
2021-07-15 11:36:21 +0900

    [UPDATE]
    메인 페이지 로고 업데이트

--- client/vue_Template01/files/components/AboutOne.vue
+++ client/vue_Template01/files/components/AboutOne.vue
@@ -5,30 +5,33 @@
         <div class="col-xl-6 col-lg-6">
           <div class="about1_img">
             <div class="about1_shape_1"></div>
-            <img src="/assets/images/about/about-1-img-1.jpg" alt="About-Img">
+            <img src="/assets/images/about/about-1-img-1.jpg" alt="About-Img" />
             <div class="about1_icon-box">
               <div class="circle">
                 <span class="icon-focus"></span>
               </div>
             </div>
             <div class="about_img_2">
-              <img src="/assets/images/about/about-1-img-2.jpg" alt="">
+              <img src="/assets/images/about/about-1-img-2.jpg" alt="" />
             </div>
           </div>
         </div>
         <div class="col-xl-6 col-lg-6">
           <div class="block-title text-left">
-            <p>About agriculture</p>
-            <h3>We’re leader in agriculture market</h3>
+            <p>About Who Cat</p>
+            <h3>반려묘 객체 인증 시스템</h3>
             <div class="leaf">
-              <img src="/assets/images/resources/leaf.png" alt="">
+              <img src="/assets/images/resources/leaf.png" alt="" />
             </div>
           </div>
           <div class="about_content">
             <div class="text">
-              <p>There are many variations of passages of Lorem Ipsum available, but the majority have
-                suffered alteration in some form, by injected humour, or randomised words which
-                don't look even slightly believable.</p>
+              <p>
+                There are many variations of passages of Lorem Ipsum available,
+                but the majority have suffered alteration in some form, by
+                injected humour, or randomised words which don't look even
+                slightly believable.
+              </p>
             </div>
             <div class="about1_icon_wrap">
               <div class="about1_icon_single">
@@ -45,11 +48,16 @@
               </div>
             </div>
             <div class="bottom_text">
-              <p> If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't
-                anything embarrassing hidden in the middle of text.</p>
+              <p>
+                If you are going to use a passage of Lorem Ipsum, you need to be
+                sure there isn't anything embarrassing hidden in the middle of
+                text.
+              </p>
             </div>
             <div class="about1__button-block">
-              <nuxt-link to="/about" class="thm-btn about_one__btn">Learn More</nuxt-link>
+              <nuxt-link to="/about" class="thm-btn about_one__btn"
+                >Learn More</nuxt-link
+              >
             </div>
           </div>
         </div>
@@ -59,11 +67,9 @@
 </template>
 
 <script>
-    export default {
-        name: "AboutOne"
-    }
+export default {
+  name: "AboutOne"
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
