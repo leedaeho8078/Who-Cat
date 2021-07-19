fb3279307f9a4654972fe39fc42168572225ce3d
JaeseungJung <86044304+JaeseungJung@users.noreply.github.com>
2021-07-19 09:43:28 +0900

    [UPDATE]

--- client/vue_Template01/files/pages/service.vue
+++ client/vue_Template01/files/pages/service.vue
@@ -5,37 +5,36 @@
     <ServicePage />
     <ServiceOne />
     <BrandOne />
-    <FaqOne />
-    <CallToActionThree />
+    <!-- <FaqOne /> -->
+    <!-- <CallToActionThree /> -->
     <Footer />
   </div>
 </template>
 
 <script>
-
-  import Nav from "../components/Nav";
-  import PageHeader from "../components/PageHeader";
-  import ServicePage from "../components/ServicePage";
-  import ServiceOne from "../components/ServiceOne";
-  import BrandOne from "../components/BrandOne";
-  import FaqOne from "../components/FaqOne";
-  import CallToActionThree from "../components/CallToActionThree";
-  import Footer from "../components/Footer";
-  export default {
-    components: {
-      Footer,
-      CallToActionThree,
-      FaqOne,
-      BrandOne,
-      ServiceOne,
-      ServicePage,
-      PageHeader,
-      Nav
-    },
-    head(){
-      return {
-        title: "Agrikol  | Services"
-      }
-    }
+import Nav from "../components/Nav";
+import PageHeader from "../components/PageHeader";
+import ServicePage from "../components/ServicePage";
+import ServiceOne from "../components/ServiceOne";
+import BrandOne from "../components/BrandOne";
+import FaqOne from "../components/FaqOne";
+import CallToActionThree from "../components/CallToActionThree";
+import Footer from "../components/Footer";
+export default {
+  components: {
+    Footer,
+    CallToActionThree,
+    FaqOne,
+    BrandOne,
+    ServiceOne,
+    ServicePage,
+    PageHeader,
+    Nav
+  },
+  head() {
+    return {
+      title: "Agrikol  | Services"
+    };
   }
+};
 </script>
