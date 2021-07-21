c81df98ef2955a05af32e958ca9c36606f8836d0
Gyubi Kim <85974080+gyubikim@users.noreply.github.com>
2021-07-21 17:14:48 +0900

    Update ProductDetail.vue

--- Who-cat2/client/vue_Template01/files/components/ProductDetail.vue
+++ Who-cat2/client/vue_Template01/files/components/ProductDetail.vue
@@ -13,7 +13,7 @@
           </div>
           <div class="col-xl-6 col-lg-6">
             <div class="product_detail_content">
-              <h2>아이디펫 외장태그(검정)</h2>
+              <h2>누구냥 외장태그(검정)</h2>
               <div class="product_detail_review_box">
                 <div class="product_detail_price_box">
                   <p>25,000원</p>
@@ -24,18 +24,18 @@
                   <a href="#"><i class="fa fa-star"></i></a>
                   <a href="#"><i class="fa fa-star"></i></a>
                   <a href="#" class="deactive"><i class="fa fa-star"></i></a>
-                  <span>2 Customer Reviews</span>
+                  <span>리뷰 2건</span>
                 </div>
               </div>
               <div class="product_detail_text">
                 <p>
-                  누구냥 로고가 인쇄돼어 있는 가죽 외장형전자태그! 뒷면에 고양이
-                  고유 ID가 인쇄되어 있어 바로 착용가능해요!
+                  누구냥 로고가 인쇄되어 있는 가죽 외장형전자태그! 뒷면에 고양이
+                  고유ID가 인쇄되어 있어 바로 착용가능해요!
                 </p>
               </div>
               <ul class="list-unstyled product_detail_address">
-                <li>REF. 4231/406</li>
-                <li>Available in store</li>
+                <li>지름 3.5cm, 두께: 2-3mm</li>
+                <li>재고있음</li>
               </ul>
               <div class="product-quantity-box">
                 <div class="input-box">
@@ -47,19 +47,19 @@
                   />
                 </div>
                 <div class="addto-cart-box">
-                  <button class="thm-btn" type="submit">Add to Cart</button>
+                  <button class="thm-btn" type="submit">장바구니</button>
                 </div>
                 <div class="wishlist_btn">
-                  <a href="#" class="thm-btn">Add to Wishlist</a>
+                  <a href="#" class="thm-btn">관심상품</a>
                 </div>
               </div>
               <ul class="list-unstyled category_tag_list">
-                <li>Category: Food</li>
-                <li>Tags: Vegetables, Fruits</li>
+                <li>최소 주문수량갯수:1</li>
+                <li>최대 주문수량갯수:3</li>
               </ul>
               <div class="product_detail_share_box">
                 <div class="share_box_title">
-                  <h2>Share with friends</h2>
+                  <h2>공유하기</h2>
                 </div>
                 <div class="share_box_social">
                   <a href="#"><i class="fab fa-facebook-square"></i></a>
@@ -88,30 +88,51 @@
                     <div class="content">
                       <div class="product-details-content">
                         <div class="desc-content-box">
+                          <img
+                            style="margin:10px"
+                            src="https://idpet.co.kr/ko_finder/userfiles/files/%EC%83%81%EC%84%B8%ED%8E%98%EC%9D%B4%EC%A7%80_%EB%8B%A8%ED%92%88_%EB%94%A5%EA%B7%B8%EB%A6%B0_%EC%99%B8%EC%9E%A5%ED%83%9C%EA%B7%B8_0628.jpg"
+                          />
+
+                          <p style="font-size:30px;color:black">
+                            회원가입 안내
+                          </p>
+                          <br />
                           <p>
-                            Lorem ipsum dolor sit amet sectetur adipiscin elit
-                            cras feuiat antesed ces condimentum viverra duis
-                            autue nim convallis id diam vitae duis egety dictum
-                            erosin dictum sem. Vivamus sed molestie sapien
-                            aliquam et facilisis arcu dut molestie augue
-                            suspendisse sodales tortor nunced quis cto ligula
-                            posuere cursus keuis aute irure dolor in
-                            reprehenderit in voluptate velit esse cillum dolore
-                            eu fugiat nulla pariatur. Excepteur sint occaecated
-                            cupidatat non proident sunt in culpa qui officia
-                            deserunt mollit anim id est laborum ivamus sed
-                            molestie sapien.
+                            회원가입 메뉴를 통해 이용약관, 개인정보보호정책 동의
+                            및 일정 양식의 가입항목을 기입함으로써 회원에
+                            가입되며, 가입 즉시 서비스를 무료로 이용하실 수
+                            있습니다. 주문하실 때에 입력해야하는 각종 정보들을
+                            일일이 입력하지 않으셔도 됩니다. 공동구매,
+                            경매행사에 항상 참여하실 수 있습니다. 회원을 위한
+                            이벤트 및 각종 할인행사에 참여하실 수 있습니다.
+                          </p>
+                          <br />
+                          <p style="font-size:30px;color:black">
+                            결제안내
                           </p>
                           <p class="desc-content-box_bottom">
-                            Aliquam et facilisis arcuut olestie augue.
-                            Suspendisse sodales tortor nunc quis auctor ligula
-                            posuere cursus duis aute irure dolor in
-                            reprehenderit in voluptate velit esse cill doloreeu
-                            fugiat nulla pariatur excepteur sint occaecat
-                            cupidatat non proident sunt in culpa qui officia
-                            deserunt mollit anim id est laborum. Vivaus sed
-                            delly molestie sapien. Aliquam et facilisis arcuut
-                            molestie augue.
+                            고액결제의 경우 안전을 위해 카드사에서 확인전화를
+                            드릴 수도 있습니다. 확인과정에서 도난 카드의
+                            사용이나 타인 명의의 주문등 정상적인 주문이 아니라고
+                            판단될 경우 임의로 주문을 보류 또는 취소할 수
+                            있습니다. 무통장 입금은 상품 구매 대금은 PC뱅킹,
+                            인터넷뱅킹, 텔레뱅킹 혹은 가까운 은행에서 직접
+                            입금하시면 됩니다. 주문시 입력한 입금자명과
+                            실제입금자의 성명이 반드시 일치하여야 하며, 7일
+                            이내로 입금을 하셔야 하며 입금되지 않은 주문은
+                            자동취소 됩니다.
+                          </p>
+                          <br />
+                          <p style="font-size:30px;color:black">
+                            환불안내
+                          </p>
+                          <p class="desc-content-box_bottom">
+                            환불시 반품 확인여부를 확인한 후 3영업일 이내에 결제
+                            금액을 환불해 드립니다. 신용카드로 결제하신 경우는
+                            신용카드 승인을 취소하여 결제 대금이 청구되지 않게
+                            합니다.(단, 신용카드 결제일자에 맞추어 대금이 청구
+                            될수 있으면 이경우 익월 신용카드 대금청구시
+                            카드사에서 환급처리 됩니다.)
                           </p>
                         </div>
                       </div>
