d66fe326fae942c54cb08310c209c6009a07f812
Gyubi Kim <85974080+gyubikim@users.noreply.github.com>
2021-07-21 10:49:39 +0900

    등록페이지

--- Who-cat2/client/vue_Template01/files/components/ProductOne.vue
+++ Who-cat2/client/vue_Template01/files/components/ProductOne.vue
@@ -4,35 +4,44 @@
       <div class="row">
         <div class="col-xl-6 col-lg-6">
           <div class="product_img">
-            <img src="/assets/images/resources/product-1-img-1.jpg" alt="Product One Img">
+            <img
+              src="/assets/images/main-slider/banner-five-bg.jpg"
+              alt="Product One Img"
+            />
             <div class="experience_box">
-              <h2>40 Year</h2>
-              <p>Of Experience</p>
+              <h2>87%</h2>
+              <p>등록시 유실묘 귀환율</p>
             </div>
           </div>
         </div>
         <div class="col-xl-6 col-lg-6">
           <div class="growing_product">
             <div class="block-title text-left">
-              <p>fresh products</p>
-              <h3>Growing products</h3>
+              <p>WHO-CAT</p>
+              <br />
+              <p style="font-size:40px;color:black">반려묘를 등록하세요!</p>
               <div class="leaf">
-                <img src="/assets/images/resources/leaf.png" alt="">
+                <img src="/assets/images/resources/leaf.png" alt="" />
               </div>
             </div>
             <div class="growing_product_text">
-              <p>Lorem ipsum dolor sit amet nsectetur cing elit. Suspe ndisse suscipit sagittis leo
-                sit met entum estibu dignissim posuere cubilia durae. Leo sit met entum cubilia crae
-                onec.</p>
+              <p>
+                온라인으로 간편하게 등록하세요! 누구냥에서는 온라인으로 간단하게
+                반려묘 등록을 진행할 수 있습니다. 사랑하는 우리 아이들을 위해
+                반려묘등록을 꼭 실천해주세요.
+              </p>
             </div>
             <div class="progress-levels">
               <!--Skill Box-->
               <div class="progress-box">
                 <div class="inner count-box">
-                  <div class="text">Agriculture</div>
+                  <div class="text">유실묘 집 귀환율</div>
                   <div class="bar">
                     <div class="bar-innner">
-                      <div class="bar-fill bar-fill-one" data-percent="68"></div>
+                      <div
+                        class="bar-fill bar-fill-one"
+                        data-percent="10"
+                      ></div>
                     </div>
                   </div>
                 </div>
@@ -40,10 +49,13 @@
               <!--Skill Box-->
               <div class="progress-box">
                 <div class="inner count-box">
-                  <div class="text">Organic</div>
+                  <div class="text">등록시 유실묘 집 귀환율</div>
                   <div class="bar">
                     <div class="bar-innner">
-                      <div class="bar-fill bar-fill-two" data-percent="98"></div>
+                      <div
+                        class="bar-fill bar-fill-two"
+                        data-percent="98"
+                      ></div>
                     </div>
                   </div>
                 </div>
@@ -57,11 +69,9 @@
 </template>
 
 <script>
-    export default {
-        name: "ProductOne"
-    }
+export default {
+  name: "ProductOne"
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
